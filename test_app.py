1479c5a559b4fff292ea11ff08cb163316b59881
ToonOwncloud <ToonOwncloud@users.noreply.github.com>
2019-12-10 14:16:38 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,10 +8,10 @@ class TestApp(unittest.TestCase):
   def setUp(self):
     app.app.testing=True
     self.app = app.app.test_client()
-'''
+
   def test_hello(self):
     self.assertTrue(hello() == "Hello World!")
-    
+'''
   def test_hello_serv(self):
     rv = self.app.get('/')
     self.assertEqual(rv.status, '200 OK')
